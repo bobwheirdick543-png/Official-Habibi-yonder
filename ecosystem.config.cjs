43e32d7169aadcbebdd91da10894f349dc5a28d5
ㅤ༺ㅤꜱᴛᴀɪɴㅤ༻ㅤ <evansken2008@gmail.com>
2026-08-03 21:05:33 +0100

    Add files via upload

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,6 @@ module.exports = {
             name: 'habibi',
             script: 'index.js',
             node_args: '--max-old-space-size=512',
-            max_memory_restart: '400M',
             autorestart: true,
             restart_delay: 3000
         }
